58dc3f448c8c10ab0a4297be3e86909a40a0e6ae
Damien <damien.philippe@edu.esiee.fr>
2025-10-20 15:10:51 +0200

    Read MSR check errors

--- src/main.rs
+++ src/main.rs
@@ -18,38 +18,60 @@ pub fn main() {
     println!("Trying to get tjMax value, should work on most Intel CPUs");
     // MSR_TEMPERATURE_TARGET
     let msr = 0x1a2;
-    let out = r0.readMsr(msr).unwrap();
- 
-    let _edx = ((out >> 32) & 0xffffffff) as u32;
-    let eax = (out & 0xffffffff) as u32;
-    let tj_max = (eax >> 16) & 0xff;
+    match r0.readMsr(msr) {
+        Ok(out) => {
+            let _edx = ((out >> 32) & 0xffffffff) as u32;
+            let eax = (out & 0xffffffff) as u32;
+            let tj_max = (eax >> 16) & 0xff;
+            println!("MSR Value: {}", tj_max);
+        },
+        Err(err) => {
+            println!("Error reading MSR: {}", err);
+        }
+    }
 
-    
+
     // MSR_RAPL_POWER_UNIT
     let msr = 0x606;
-    // The 64 bit value returned into EDX:EAX registers
-    let out = r0.readMsr(msr).unwrap();
-    // Split the 64 bit value into the original by offsetting and masking
-    // EDX: high order 32 bits
-    let edx = ((out >> 32) & 0xffffffff) as u32;
-    // EAX: low order 32 bits
-    let eax = (out & 0xffffffff) as u32;
-    // power_unit = 1/2^PU where PU is bits 3:0 of EAX
-    let power_unit = 1.0 / f64::from(1 << (eax & 0xf));
-    // energy_unit = 1/2^EU where EU is bits 12:8 of EAX
-    let energy_unit = 1.0 / f64::from(1 << ((eax >> 8) & 0x1f));
-    // time_unit = 1/2^TU where TU is bits 19:16 of EAX
-    let time_unit = 1.0 / f64::from(1 << ((eax >> 16) & 0xf));
-    
-    println!("Raw Power Unit: {:b}", eax & 0xf);
-    println!("Raw Energy Unit: {:b}", (eax >> 8) & 0x1f);
-    println!("Raw Time Unit: {:b}", (eax >> 16) & 0xf);
+    match r0.readMsr(msr) {
+        Ok(out) => {
+            // Split the 64 bit value into the original by offsetting and masking
+            // EDX: high order 32 bits
+            let edx = ((out >> 32) & 0xffffffff) as u32;
+            // EAX: low order 32 bits
+            let eax = (out & 0xffffffff) as u32;
+            // power_unit = 1/2^PU where PU is bits 3:0 of EAX
+            let power_unit = 1.0 / f64::from(1 << (eax & 0xf));
+            // energy_unit = 1/2^EU where EU is bits 12:8 of EAX
+            let energy_unit = 1.0 / f64::from(1 << ((eax >> 8) & 0x1f));
+            // time_unit = 1/2^TU where TU is bits 19:16 of EAX
+            let time_unit = 1.0 / f64::from(1 << ((eax >> 16) & 0xf));
+            
+            println!("Raw Power Unit: {:b}", eax & 0xf);
+            println!("Raw Energy Unit: {:b}", (eax >> 8) & 0x1f);
+            println!("Raw Time Unit: {:b}", (eax >> 16) & 0xf);
 
-    println!("Power Unit: {}mW", power_unit);
-    println!("Energy Unit: {}µJ", energy_unit);
-    println!("Time Unit: {}s", time_unit);
- 
-    println!("MSR Value: {}", tj_max);
+            println!("Power Unit: {}mW", power_unit);
+            println!("Energy Unit: {}µJ", energy_unit);
+            println!("Time Unit: {}s", time_unit);
+        },
+        Err(err) => {
+            println!("Error reading MSR: {}", err);
+            return;
+        }
+    }
+
+    // let mut count = 0;
+    // while count < 100 {
+    //     // MSR_PKG_ENERGY_STATUS
+    //     let msr = 0x611;
+    //     let out = r0.readMsr(msr).unwrap();
+    //     let edx = ((out >> 32) & 0xffffffff) as u32;
+    //     let eax = (out & 0xffffffff) as u32;
+    //     let energy = ((edx as u64) << 32) | (eax as u64);
+    //     println!("Energy: {}", energy);
+    //     count += 1;
+    // }  
  
     println!("Closing ring0 driver");
     match r0.close() {
